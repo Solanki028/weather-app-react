3de718f04e753b87c1b76d64ca1a857daf052b67
unknown <you@example.com>
2025-01-09 17:32:29 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+ 
+import './App.css';
+import Temp from './component/weather/temp';
+
+function App() {
+  return (
+     
+    <div>
+      <Temp/>
+    </div>
+    
+  );
+}
+
+export default App;
